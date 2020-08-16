b86768dbb383c0f0e17c5102dad4b542b831970d
Fan Yang <yangfcm16@gmail.com>
2020-08-16 15:09:08 +0930

    update test

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Algorithms Problems and Solutions
+
+## Intro
+
+Interview: Algorithms problems and solutions written in Java, including test cases.
+
+## Algorithms problems content
+
+- All problems and solutions are in directory: `src/main/java/algo/*`
+- All test cases are in directory: `src/test/java/algo/*`
+
+`/mathematics`
+
+- **Is Prime** Test if a given number is a prime - `IsPrime.cs`
+
+## Development Environment
+
+- Java 11
+- [Gradle](https://docs.gradle.org/current/userguide/installation.html)
+- [Junit5](https://junit.org/junit5/docs/current/user-guide/#overview)
+
+## How to run
+
+Under project root directory: `gradle test`.
--- src/test/java/algo/mathematics/IsPrimeTest.java
+++ src/test/java/algo/mathematics/IsPrimeTest.java
@@ -1,11 +1,20 @@
 package algo.mathematics;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IsPrimeTest {
-  @Test
-  void test2NotPrime() {
-    assertTrue(IsPrime.run(2), "2 is a prime");
+
+  @ParameterizedTest(name = "Number {0} is tested as a prime number")
+  @ValueSource(ints = { 2, 3, 17, 8747 })
+  void testPrime(int num) {
+    assertTrue(IsPrime.run(num));
+  }
+
+  @ParameterizedTest(name = "Number {0} is tested as a non-prime number")
+  @ValueSource(ints = { 4, 8, 522 })
+  void testNonPrime(int num) {
+    assertFalse(IsPrime.run(num));
   }
 }

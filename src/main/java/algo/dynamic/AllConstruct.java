01e779f0bbaf70d2e90afc45cfd14be9f8c4e139
Fan Yang <yangfcm16@gmail.com>
2021-09-02 23:05:15 +0930

    add AllConstruct problem and solutions.

--- src/main/java/algo/dynamic/AllConstruct.java
+++ src/main/java/algo/dynamic/AllConstruct.java
@@ -1,5 +1,113 @@
 package algo.dynamic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @name AllConstruct
+ * @description Given a target string and an array of strings.
+ * The function should return a list of arrays containing all of the ways that the target can be
+ * constructed by concatenating elements of the strs array.
+ * You may use elements in strs as many times as needed.
+ */
 public class AllConstruct {
-  
+
+  /**
+   * solution1
+   * @param target
+   * @param strs
+   * @return A list of all combinations to get the target string
+   * @implNote Use recursive
+   */
+  static public ArrayList<ArrayList<String>> solution1(String target, String[] strs) {
+    if(target.equals("")) { 
+      return new ArrayList<ArrayList<String>>(){{
+        add(new ArrayList<String>());
+      }};
+    }
+    ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>(); 
+    for(String str: strs) {
+      if(target.indexOf(str) == 0) {
+        var suffix = target.substring(str.length());
+        var suffixWays = solution1(suffix, strs);
+        for(ArrayList<String> way: suffixWays) {
+          way.add(0, str);
+          result.add(way);
+        }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * solution2
+   * @param target
+   * @param strs
+   * @return A list of all combinations to get the target string
+   * @implNote Use recursive + memoization
+   */
+  static public ArrayList<ArrayList<String>> solution2(String target, String[] strs) {
+    return solution2(target, strs, new HashMap<String, ArrayList<ArrayList<String>>>());
+  }
+
+  static public ArrayList<ArrayList<String>> solution2(
+    String target, 
+    String[] strs, 
+    Map<String, ArrayList<ArrayList<String>>> memo) {
+      if(target.equals("")) { 
+        return new ArrayList<ArrayList<String>>(){{
+          add(new ArrayList<String>());
+        }};
+      }
+      if(memo.containsKey(target)) return memo.get(target);
+      ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>(); 
+      for(String str: strs) {
+        if(target.indexOf(str) == 0) {
+          String suffix = target.substring(str.length());
+          ArrayList<ArrayList<String>> suffixWays = solution2(suffix, strs, memo);
+          for(ArrayList<String> way: suffixWays) {
+            way.add(0, str);
+            result.add(way);
+          }
+        }
+      }
+
+      // deep clone the result and add it to memo map.
+      var copyResult = new ArrayList<ArrayList<String>>();
+      for(int i = 0; i < result.size(); i++) {
+        copyResult.add(new ArrayList<String>(result.get(i)));
+      }
+      memo.put(target, copyResult);
+      return result;
+  }
+
+  /**
+   * solution3
+   * @param target
+   * @param strs
+   * @return A list of all combinations to get the target string
+   * @implNote Use recursive + tabulation
+   */
+  static public ArrayList<ArrayList<String>> solution3(String target, String[] strs) {
+    var table =  (ArrayList<ArrayList<String>>[]) new ArrayList[target.length()+1];
+    for(int i = 0; i <= target.length(); i++) {
+      table[i] = new ArrayList<ArrayList<String>>();
+    }
+    table[0].add(new ArrayList<String>());
+    for(int i = 0; i <= target.length(); i++) {
+      for(String str: strs) {
+        if(i + str.length() <= target.length() && target.substring(i, i+str.length()).equals(str)) {
+          for(ArrayList<String> sub: table[i]) {
+            var newSub = new ArrayList<String>(sub) {
+              {add(str);}
+            };
+            table[i + str.length()].add(newSub);
+          }
+        }
+      }
+    }
+    return table[target.length()];
+  }
 }
---
+++ src/test/java/algo/dynamic/AllConstructTest.java
@@ -0,0 +1,119 @@
+package algo.dynamic;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("Test Dynamic/AllConstruct")
+public class AllConstructTest {
+
+  @ParameterizedTest(name = "solution one: the all possible combinations for \"{0}\" from {1} can be found")
+  @MethodSource("allConstructSourceForSolution1")
+  void testSolutionOne(String target, String[] strs, ArrayList<ArrayList<String>> expected){
+    var actual = AllConstruct.solution1(target, strs);
+    assertTrue(assert2DListsEqual(actual, expected));
+  }
+
+  @ParameterizedTest(name = "solution two: the all possible combinations for \"{0}\" from {1} can be found")
+  @MethodSource("allConstructSourceForSolution2")
+  void testSolutionTwo(String target, String[] strs, ArrayList<ArrayList<String>> expected){
+    var actual = AllConstruct.solution2(target, strs);
+    assertTrue(assert2DListsEqual(actual, expected));
+  }
+
+  @ParameterizedTest(name = "solution three: the all possible combinations for \"{0}\" from {1} can be found")
+  @MethodSource("allConstructSourceForSolution3")
+  void testSolutionThree(String target, String[] strs, ArrayList<ArrayList<String>> expected){
+    var actual = AllConstruct.solution3(target, strs);
+    assertTrue(assert2DListsEqual(actual, expected));
+  }
+  
+  static Stream<Arguments> allConstructSourceForSolution1() {
+    return Stream.of(
+      Arguments.arguments(
+        "purple", 
+        new String[] { "purp", "p", "ur", "le", "purpl" },
+        new ArrayList<ArrayList<String>>(Arrays.asList(
+          new ArrayList<String>(Arrays.asList("purp", "le")),
+          new ArrayList<String>(Arrays.asList("p", "ur", "p", "le"))
+        ))
+      ),
+      Arguments.arguments(
+        "abcdef",
+        new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"},
+        new ArrayList<ArrayList<String>>(Arrays.asList(
+          new ArrayList<String>(Arrays.asList("ab", "cd", "ef")),
+          new ArrayList<String>(Arrays.asList("ab", "c", "def")),
+          new ArrayList<String>(Arrays.asList("abc", "def")),
+          new ArrayList<String>(Arrays.asList("abcd", "ef"))
+        ))
+      )
+    );
+  }
+
+  static Stream<Arguments> allConstructSourceForSolution2() {
+    return Stream.of(
+      Arguments.arguments(
+        "purple", 
+        new String[] { "purp", "p", "ur", "le", "purpl" },
+        new ArrayList<ArrayList<String>>(Arrays.asList(
+          new ArrayList<String>(Arrays.asList("purp", "le")),
+          new ArrayList<String>(Arrays.asList("p", "ur", "p", "le"))
+        ))
+      ),
+      Arguments.arguments(
+        "abcdef",
+        new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"},
+        new ArrayList<ArrayList<String>>(Arrays.asList(
+          new ArrayList<String>(Arrays.asList("ab", "cd", "ef")),
+          new ArrayList<String>(Arrays.asList("ab", "c", "def")),
+          new ArrayList<String>(Arrays.asList("abc", "def")),
+          new ArrayList<String>(Arrays.asList("abcd", "ef"))
+        ))
+      )
+    );
+  }
+
+  static Stream<Arguments> allConstructSourceForSolution3() {
+    return Stream.of(
+      Arguments.arguments(
+        "purple", 
+        new String[] { "purp", "p", "ur", "le", "purpl" },
+        new ArrayList<ArrayList<String>>(Arrays.asList(
+          new ArrayList<String>(Arrays.asList("purp", "le")),
+          new ArrayList<String>(Arrays.asList("p", "ur", "p", "le"))
+        ))
+      ),
+      Arguments.arguments(
+        "abcdef",
+        new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"},
+        new ArrayList<ArrayList<String>>(Arrays.asList( // The order is different.
+          new ArrayList<String>(Arrays.asList("abc", "def")),
+          new ArrayList<String>(Arrays.asList("ab", "c", "def")),
+          new ArrayList<String>(Arrays.asList("abcd", "ef")),
+          new ArrayList<String>(Arrays.asList("ab", "cd", "ef"))
+        ))
+      )
+    );
+  }
+
+  // static Stream<Arguments> allConstructSourceWithBig() {}
+
+  private static <T> boolean assert2DListsEqual(ArrayList<ArrayList<T>> list1, ArrayList<ArrayList<T>> list2) {
+    if(list1.size() != list2.size()) return false;
+    for(int i = 0; i < list1.size(); i++) {
+      if(!list1.get(i).equals(list2.get(i))) return false;
+    }
+    return true;
+  } 
+}

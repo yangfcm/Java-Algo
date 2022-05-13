7932855c4e665c4b8aaa042ec31d6a7762dfcf34
Fan Yang <yangfcm16@gmail.com>
2022-05-14 00:42:14 +0930

    search the n-th node from linked list.

---
+++ src/main/java/algo/ds/linkedlist/SearchFromLast.java
@@ -0,0 +1,21 @@
+package algo.ds.linkedlist;
+
+/**
+ * @name SearchFromLast
+ * @description Given a linked list and a number n, return the element n spaces from the last node in the list.
+ */
+public class SearchFromLast {
+  static public Node solution(MyLinkedList list, int n) {
+    var slow = list.getHead();
+    var fast = list.getHead();
+    for(int i = 0; i < n; i++) {
+      // Move fast advance by n steps.
+      fast = fast.next;
+    }
+    while(fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return slow;
+  }
+}
---
+++ src/test/java/algo/ds/linkedlist/SearchFromLastTest.java
@@ -0,0 +1,40 @@
+package algo.ds.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Test ds/linkedlist/SearchFromLast")
+public class SearchFromLastTest {
+  private MyLinkedList<Integer> list;
+
+  @BeforeEach
+  public void setUp() {
+    list = new MyLinkedList<Integer>();
+    list.addLast(1);
+    list.addLast(2);
+    list.addLast(3);
+    list.addLast(4);
+    list.addLast(5);
+    list.addLast(6);
+  }
+
+  @AfterEach
+  public void clean() {
+    list = null;
+  }
+
+  @DisplayName("Should be able to find out element from the last")
+  @Test
+  public void testSearchFromLast() {
+    assertEquals(6, SearchFromLast.solution(list, 0).getData());
+    assertEquals(5, SearchFromLast.solution(list, 1).getData());
+    assertEquals(4, SearchFromLast.solution(list, 2).getData());
+    assertEquals(3, SearchFromLast.solution(list, 3).getData());
+    assertEquals(2, SearchFromLast.solution(list, 4).getData());
+    assertEquals(1, SearchFromLast.solution(list, 5).getData());
+  }
+}

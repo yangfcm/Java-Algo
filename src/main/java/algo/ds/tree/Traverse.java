4c1dc70c5018468f606c23fb5bb6e784f4e51a25
Fan Yang <yangfcm16@gmail.com>
2022-05-15 23:56:26 +0930

    Implement dfs traverse for tree.

--- src/main/java/algo/ds/tree/Traverse.java
+++ src/main/java/algo/ds/tree/Traverse.java
@@ -13,24 +13,33 @@ public class Traverse {
   public static List<Object> bfs(Tree<Object> tree) {
     traverseList = new ArrayList<Object>();
     var tempList = new ArrayList<Tree<Object>>();
-    traverseList.add(tree.getData());
     tempList.add(tree);
     while(tempList.size() > 0) {
       Tree<Object> firstNode = tempList.get(0); // Save the current node.
       tempList.remove(0);
 
       for (Tree<Object> childNode : firstNode.getChildren()) {
-        traverseList.add(childNode.getData());
         tempList.add(childNode);
       }
+      traverseList.add(firstNode.getData());
     }
     return traverseList;
   }
 
-  public List<Object> dfs() {
+  public static List<Object> dfs(Tree<Object> tree) {
     traverseList = new ArrayList<Object>();
-    // @TODO: implement DFS search.
+    var tempList = new ArrayList<Tree<Object>>();
+    tempList.add(tree);
+    while(tempList.size() > 0) {
+      Tree<Object> firstNode = tempList.get(0);
+      List<Tree<Object>> children = firstNode.getChildren();
+      tempList.remove(0);
 
+      for(int i = children.size() - 1; i >= 0; i--) {
+        tempList.add(0, children.get(i));
+      }
+      traverseList.add(firstNode.getData());
+    }
     return traverseList;
   }
 }
--- src/test/java/algo/ds/tree/TraverseTest.java
+++ src/test/java/algo/ds/tree/TraverseTest.java
@@ -40,4 +40,16 @@ public class TraverseTest {
     assertEquals('g', traverseList.get(6));
   }
 
+  @DisplayName("Should be able to do DFS traverse")
+  @Test
+  public void testDfs() {
+    var traverseList = Traverse.dfs(tree);
+    assertEquals('a', traverseList.get(0));
+    assertEquals('b', traverseList.get(1));
+    assertEquals('e', traverseList.get(2));
+    assertEquals('f', traverseList.get(3));
+    assertEquals('c', traverseList.get(4));
+    assertEquals('d', traverseList.get(5));
+    assertEquals('g', traverseList.get(6));
+  }
 }

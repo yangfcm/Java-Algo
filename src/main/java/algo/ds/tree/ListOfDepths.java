5e6b10b925f535c6041514d92e70c18b94f21238
Fan Yang <yangfcm16@gmail.com>
2022-05-17 21:12:26 +0930

    Implement list of depths and test it.

--- src/main/java/algo/ds/tree/ListOfDepths.java
+++ src/main/java/algo/ds/tree/ListOfDepths.java
@@ -1,4 +1,39 @@
 package algo.ds.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @name ListOfDepths
+ * @description  Given a binary(not necessarily balanced) tree, create an array/list of all the nodes at each level.
+ * If you have a tree with N levels, you'll have N arrays/lists.
+ * --- Example
+ * Given:
+ *      0
+ *    /   \
+ *   1     3
+ *  / \   /
+ * 4  6  5
+ * Answer: [[Node(0)], [Node(1), Node(3)], [Node(4), Node(6), Node(5)]]
+ */
 public class ListOfDepths {
+  public static List<List<Bst>> solution(Bst root) {
+    List<List<Bst>> lists = new ArrayList<List<Bst>>();
+    createLevelListHelper(root, lists, 0);
+    return lists;
+  }
+
+  private static void createLevelListHelper(Bst root, List<List<Bst>> lists, int level ) {
+    if(root == null) return;
+    List<Bst> list;
+    if(lists.size() == level) {
+      list = new ArrayList<Bst>();
+      lists.add(list);
+    } else {
+      list = lists.get(level);
+    }
+    list.add(root);
+    createLevelListHelper(root.getLeft(), lists, level + 1);
+    createLevelListHelper(root.getRight(), lists, level + 1);
+  }
 }
--- src/test/java/algo/ds/tree/ListOfDepthsTest.java
+++ src/test/java/algo/ds/tree/ListOfDepthsTest.java
@@ -1,5 +1,42 @@
 package algo.ds.tree;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Test ds/tree/ListOfDepths")
 public class ListOfDepthsTest {
+  private Bst node;
   
+  @BeforeEach
+  public void setUp() {
+    node = new Bst(5);
+    node.add(3);
+    node.add(7);
+    node.add(2);
+    node.add(4);
+    node.add(6);
+    node.add(1);
+  }
+
+  @AfterEach
+  public void clean() {
+    node = null;
+  }
+
+  @DisplayName("Should be able to create lists")
+  @Test
+  public void testSolution() {
+    var lists = ListOfDepths.solution(node);
+    assertEquals(5, lists.get(0).get(0).getData());
+    assertEquals(3, lists.get(1).get(0).getData());
+    assertEquals(7, lists.get(1).get(1).getData());
+    assertEquals(2, lists.get(2).get(0).getData());
+    assertEquals(4, lists.get(2).get(1).getData());
+    assertEquals(6, lists.get(2).get(2).getData());
+    assertEquals(1, lists.get(3).get(0).getData());
+  }
 }

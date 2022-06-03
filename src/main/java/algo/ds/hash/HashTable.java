74f700100a0bdcfa1878752b37ceb6afde276aa6
Fan Yang <yangfcm16@gmail.com>
2022-06-03 13:33:35 +0930

    Remove redundant word.

--- src/main/java/algo/ds/hash/HashTable.java
+++ src/main/java/algo/ds/hash/HashTable.java
@@ -10,7 +10,7 @@ import java.util.Objects;
  * @note When implementing hash table in Java, there's some difference in terms of the language features.
  * 1. Java doesn't have built-in data structure that allows you to store multiple different typed values. 
  * In dynamic languages, like JS, python, it's not an issue; In typed language(C# to be more specific), it actually provides *tuples* that is used to solve this.
- * So to tackle tackle issue, you'll have to create your own data structure, called *HashNode* in this case.
+ * So to tackle issue, you'll have to create your own data structure, called *HashNode* in this case.
  * 2. Java doesn't support generic array like C#. To solve this, use array list instead of array
  */
 public class HashTable<K, V> {

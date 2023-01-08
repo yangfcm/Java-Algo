ec718d45d7f2b537dfe83d2c30ee38c5032893c7
Fan Yang <yangfcm16@gmail.com>
2023-01-08 21:49:57 +1000

    Add comment to IsBalanced.

--- src/main/java/algo/ds/stack_queue/IsBalanced.java
+++ src/main/java/algo/ds/stack_queue/IsBalanced.java
@@ -2,6 +2,17 @@ package algo.ds.stack_queue;
 
 import java.util.Stack;
 
+/**
+ * @name IsBalanced
+ * @description Given a string, return true or false to indicate whether the string has balanced brackets.
+ * Brackets can be '[]', '{}', or '()'
+ * @example s = "{[()]}" -> true
+ * s = "{[(])} " -> false
+ * s = "{{[[(())]]}}" -> true
+ * s = "abcd" -> true  If there is no brackets, we would consider it as balanced. 
+ * @solution Use the concept of stack
+ * @source https://www.hackerrank.com/challenges/balanced-brackets/problem
+ */
 public class IsBalanced {
 
   public static boolean solution(String str) {
